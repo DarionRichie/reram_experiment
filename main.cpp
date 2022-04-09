5bb9ce1eb8779f5a9ca6cc51df46cb7826debf29
dtwang <2969524644@qq.com>
2022-04-10 00:46:51 +0800

    feat: format the code

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ bool running = false;
 
 void thread_scanner_func()
 {
-    while (running) {
+    while (running)
+    {
         // 扫描出需要修复的 segments
         std::tuple<std::vector<std::string>, std::vector<int>, std::vector<int>, std::unordered_map<std::string, int>> tuple = manager->scan_corrupted_segments();
         std::vector<std::string> &segment_ids = std::get<0>(tuple);
@@ -23,7 +24,8 @@ void thread_scanner_func()
         // segments 排序
         storj::data_manager::sort_segments(segment_ids, ks, rs);
         // 修复 segments
-        for (const auto &segment_id: segment_ids) {
+        for (const auto &segment_id : segment_ids)
+        {
             manager->repair_segment(segment_id);
         }
 
@@ -36,29 +38,35 @@ bool compare_file(const std::string &filename1, const std::string &filename2)
 {
     int fd1 = open(filename1.c_str(), O_RDONLY);
     int fd2 = open(filename2.c_str(), O_RDONLY);
-    if (fd1 == -1 || fd2 == -1) {
+    if (fd1 == -1 || fd2 == -1)
+    {
         perror("Failed to open file");
         return false;
     }
     const int unit = 10 << 2;
-    char buf1[unit] = { 0 };
-    char buf2[unit] = { 0 };
+    char buf1[unit] = {0};
+    char buf2[unit] = {0};
     int n1;
     int n2;
-    while (true) {
+    while (true)
+    {
         n1 = read(fd1, buf1, unit);
         n2 = read(fd2, buf2, unit);
-        if (n1 != n2) {
+        if (n1 != n2)
+        {
             close(fd1);
             close(fd2);
             puts("111");
             return false;
         }
-        if (n1 <= 0) {
+        if (n1 <= 0)
+        {
             break;
         }
-        for (int i = 0; i < unit; i++) {
-            if (buf1[i] != buf2[i]) {
+        for (int i = 0; i < unit; i++)
+        {
+            if (buf1[i] != buf2[i])
+            {
                 close(fd1);
                 close(fd2);
                 puts("222");
@@ -76,15 +84,18 @@ void create_file(int size)
     const int unit = 10 << 2;
     // Write the file
     int fd = open(FILENAME_IN.c_str(), O_CREAT | O_RDWR);
-    if (fd == -1) {
+    if (fd == -1)
+    {
         perror("Failed to open file");
         return;
     }
     char buf[unit];
-    for (int i = 0; i < unit; i++) {
+    for (int i = 0; i < unit; i++)
+    {
         buf[i] = i % 10 + '0';
     }
-    for (int i = 0; i < size; i += unit) {
+    for (int i = 0; i < size; i += unit)
+    {
         write(fd, buf, std::min(unit, size - i));
     }
     close(fd);
@@ -95,20 +106,21 @@ int main(int argc, char **argv)
     manager = new storj::data_manager();
 
     storj::config cfg;
-   // cfg.file_size = 5* 1024 * 1024 ;
-   // cfg.segment_size = 1 * 1024 * 1024;
-   // cfg.stripe_size = 1024 * 1024;
+    // cfg.file_size = 5* 1024 * 1024 ;
+    // cfg.segment_size = 1 * 1024 * 1024;
+    // cfg.stripe_size = 1024 * 1024;
 
-    if (argc != 7) {
-	std::cout << "check argv !!!! -- usage -- exec file_size segment_size stripe_size k m n" << std::endl;
-    	exit(0);
+    if (argc != 7)
+    {
+        std::cout << "check argv !!!! -- usage -- exec file_size segment_size stripe_size k m n" << std::endl;
+        exit(0);
     }
 
     std::cout << std::atoi(argv[1]) << std::endl;
-    cfg.file_size = std::atoi(argv[1]) ;
+    cfg.file_size = std::atoi(argv[1]);
     cfg.segment_size = std::atoi(argv[2]);
-    cfg.stripe_size = std::atoi(argv[3]) ;
-    std::cout<<"file_size : "<<cfg.file_size<<" segment_size : "<<cfg.segment_size<<" stripe: size "<<cfg.stripe_size<<std::endl;
+    cfg.stripe_size = std::atoi(argv[3]);
+    std::cout << "file_size : " << cfg.file_size << " segment_size : " << cfg.segment_size << " stripe: size " << cfg.stripe_size << std::endl;
 
     // cfg.erasure_share_size = 512;
 
@@ -116,7 +128,7 @@ int main(int argc, char **argv)
     cfg.m = std::atoi(argv[5]);
     cfg.n = std::atoi(argv[6]);
 
-    std::cout << cfg.k << " is k  "<< cfg.m << " is m  " << cfg.n << " is n" << std::endl;
+    std::cout << cfg.k << " is k  " << cfg.m << " is m  " << cfg.n << " is n" << std::endl;
     create_file(cfg.file_size);
     puts("file created");
 
@@ -135,20 +147,23 @@ int main(int argc, char **argv)
         // 写文件
         const int unit = 10 << 2;
         int fd = open(FILENAME_OUT.c_str(), O_CREAT | O_RDWR);
-        if (fd == -1) {
+        if (fd == -1)
+        {
             perror("Failed to open file");
             return 1;
         }
-        for (int i = 0; i < file.segments.size(); i++) {
+        for (int i = 0; i < file.segments.size(); i++)
+        {
             const storj::segment &segment = file.segments[i];
 
-           // std::cout << i << std::endl;
+            // std::cout << i << std::endl;
 
-            for (int j = 0; j < segment.data.size(); j += unit) {
-                int size = std::min(unit, (int) segment.data.size() - j);
+            for (int j = 0; j < segment.data.size(); j += unit)
+            {
+                int size = std::min(unit, (int)segment.data.size() - j);
 
                 // 写到最后一个 segment 时，可能有冗余数据
-                //if (i == file.segments.size() - 1) {
+                // if (i == file.segments.size() - 1) {
                 //    size = std::min(size, cfg.file_size % cfg.segment_size - j);
                 //}
                 write(fd, segment.data.data() + j, size);
@@ -159,9 +174,12 @@ int main(int argc, char **argv)
     }
 
     // 比较上传与下载的文件内容是否相同
-    if (compare_file(FILENAME_IN, FILENAME_OUT)) {
+    if (compare_file(FILENAME_IN, FILENAME_OUT))
+    {
         std::cout << "Same" << std::endl;
-    } else {
+    }
+    else
+    {
         std::cout << "!!! DIFFERENT !!!" << std::endl;
     }
 
--- test_main.cpp
+++ test_main.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 #include "storj/data_manager.h"
 
-#include<vector>
+#include <vector>
 #include <sstream>
 
 #include "storj/data_processor.h"
@@ -20,22 +20,22 @@ bool running = true;
 
 // int Find_erasure_size()
 // {
-//     std::string file = "./test_data.txt"; 
+//     std::string file = "./test_data.txt";
 //     std::ifstream ifs(file.c_str(), std::fstream::in);
 
 //     if(!ifs)
-//         std::cout << "Read file failed!" << std::endl;   
+//         std::cout << "Read file failed!" << std::endl;
 //     //obtain size of file in Byte
 //     ifs.seekg(0, ifs.end);
 //     int length = ifs.tellg();
-//     std::cout << "length: " << length << std::endl; 
+//     std::cout << "length: " << length << std::endl;
 
 //     std::string line;
 //     //check '\n' from second character because the last character is '\n'
-//     int index = -2; 
+//     int index = -2;
 //     int linenum = 0;
 //     while(length)
-//     {   
+//     {
 //         char c;
 //         ifs.seekg (index, ifs.end);
 //         ifs.get(c);
@@ -58,64 +58,64 @@ bool running = true;
 
 void thread_scanner_func()
 {
-    //int reram_reduce = 0;
-    while (running) {
+    // int reram_reduce = 0;
+    while (running)
+    {
         std::cout << "new loop !\n";
         // 扫描出需要修复的 segments
-        std::tuple<std::vector<std::string>, std::vector<int>, std::vector<int>,std::unordered_map<std::string,int>> tuple = manager->scan_corrupted_segments();
+        std::tuple<std::vector<std::string>, std::vector<int>, std::vector<int>, std::unordered_map<std::string, int>> tuple = manager->scan_corrupted_segments();
         std::vector<std::string> &segment_ids = std::get<0>(tuple);
         std::vector<int> &ks = std::get<1>(tuple);
         std::vector<int> &rs = std::get<2>(tuple);
-	std::unordered_map<std::string,int> &corrupted_segmetn_size = std::get<3>(tuple);
+        std::unordered_map<std::string, int> &corrupted_segmetn_size = std::get<3>(tuple);
         // segments 排序
         storj::data_manager::sort_segments(segment_ids, ks, rs);
         //修复 segments
-	std::cout << "scan the segment size is : " << segment_ids.size() << std::endl;
-	if (segment_ids.size() == 0) {
-		break;
-	}
-         //for (const auto &segment_id: segment_ids) {
-         //    manager->repair_segment(segment_id);
+        std::cout << "scan the segment size is : " << segment_ids.size() << std::endl;
+        if (segment_ids.size() == 0)
+        {
+            break;
+        }
+        // for (const auto &segment_id: segment_ids) {
+        //     manager->repair_segment(segment_id);
         //  }
-        //打印需要的semgents -> 
-        for (const auto &  one_segment : segment_ids) {
+        //打印需要的semgents ->
+        for (const auto &one_segment : segment_ids)
+        {
             std::cout << "this segment must be repair : " << one_segment << std::endl;
-          //  clock_t start,stop;
-           // double duration;
-           // start=clock();
-            
-         std::ofstream mycout("test_data.txt",std::ios::app);
-         auto iter = corrupted_segmetn_size.begin();
-	 while(iter!=corrupted_segmetn_size.end())
-	 {
-		 mycout << "file & corrupted size"<< iter -> first << "," << iter -> second <<std::endl;
-		++iter; 
-	 }
+            //  clock_t start,stop;
+            // double duration;
+            // start=clock();
 
+            std::ofstream mycout("test_data.txt", std::ios::app);
+            auto iter = corrupted_segmetn_size.begin();
+            while (iter != corrupted_segmetn_size.end())
+            {
+                mycout << "file & corrupted size" << iter->first << "," << iter->second << std::endl;
+                ++iter;
+            }
 
-         manager-> repair_segment(one_segment);
-        //    stop=clock();
-        //    duration=((double)(stop-start))/CLOCK_TAI;
-        //    std::cout<<"!time:"<<duration<<std::endl;
+            manager->repair_segment(one_segment);
+            //    stop=clock();
+            //    duration=((double)(stop-start))/CLOCK_TAI;
+            //    std::cout<<"!time:"<<duration<<std::endl;
 
-
-           // std::ofstream mycout("test_data.txt",std::ios::app);
+            // std::ofstream mycout("test_data.txt",std::ios::app);
             // int erasure_size=Find_erasure_size()
-	    // //int reram_time = std::min(erasure_size*8/512,1);
-           // mycout<<"Jerasure Repair time:"<<duration<<std::endl;
-           // mycout<<"Reram Repair time:"<<duration+reram_time-reram_reduce<<std::endl;
-            //mycout<<"Reram Repair time:"<<duration+reram_time<<std::endl;
-           // mycout.close();
+            // //int reram_time = std::min(erasure_size*8/512,1);
+            // mycout<<"Jerasure Repair time:"<<duration<<std::endl;
+            // mycout<<"Reram Repair time:"<<duration+reram_time-reram_reduce<<std::endl;
+            // mycout<<"Reram Repair time:"<<duration+reram_time<<std::endl;
+            // mycout.close();
         }
         // 等待 30s
-	sleep(30);
-       // std::this_thread::sleep_for(std::chrono::seconds(10));
+        sleep(30);
+        // std::this_thread::sleep_for(std::chrono::seconds(10));
     }
 }
 
-
-
-int main() {
+int main()
+{
     // std::thread t2(thread_scanner_func);
     // t2.join();
     thread_scanner_func();
